b456052574ebf6c39d38cb109d8ab84a0b4b03af
Philippe Horne <phorne@uncharted.software>
2017-07-11 17:05:02 -0400

    Pared down the docker file and fixed errors generated during ingest.

--- server/ingest.sh
+++ server/ingest.sh
@@ -9,5 +9,5 @@ echo $DATASETS
 for dataset in "${DATASETS[@]}"
 do
     ds=${dataset/_}
-    ./distil-ingest -database=distil -db-table $ds -db-user=distil -db-password=gopher! -clear-existing -dataset-path /input/$dataset
+    ./distil-ingest -database=distil -db-table $ds -db-user=distil -db-password=gopher! -dataset-path /input/$dataset -clear-existing=false
 done
--- server/start_services.sh
+++
@@ -1,2 +0,0 @@
-echo "Runnnig postgres"
-postgres&
