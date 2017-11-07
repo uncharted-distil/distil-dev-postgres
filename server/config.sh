16f13899239294d9498abe7a142709e31ed15890
Philippe Horne <phorne@uncharted.software>
2017-11-07 16:19:55 -0500

    Version bumped.

--- server/config.sh
+++ server/config.sh
@@ -2,7 +2,7 @@
 
 # name and version of docker image that will be created
 DOCKER_IMAGE_NAME=distil_dev_postgres
-DOCKER_IMAGE_VERSION=0.5.0
+DOCKER_IMAGE_VERSION=0.5.1
 
 # datasets to ingest
 DATASETS=(r_26 r_27 r_32 r_60 o_185 o_196 o_313 o_38 o_4550)
