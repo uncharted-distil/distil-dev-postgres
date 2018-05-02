6ef5f65d289372c6a640195d4299916f47f796d5
Philippe Horne <phorne@uncharted.software>
2018-05-02 13:54:30 -0400

    Data directory renamed to map to git repo.

--- server/config.sh
+++ server/config.sh
@@ -8,7 +8,7 @@ DOCKER_IMAGE_VERSION=0.8.1
 DATASETS=(26_radon_seed 32_wikiqa 60_jester 185_baseball 196_autoMpg 313_spectrometer 38_sick 1491_one_hundred_plants_margin 27_wordLevels 57_hypothyroid 299_libras_move 534_cps_85_wages 1567_poker_hand)
 
 # path to data on local system (ingest from HDFS not currently supported)
-HOST_DATA_DIR=~/data/d3m_new
+HOST_DATA_DIR=~/datasets/seed_datasets_current
 
 # path to data in the docker container
 CONTAINER_DATA_DIR=/input/d3m
