edae64b193f84941fb8934b6ba543eb2d505b14a
Philippe Horne <phorne@uncharted.software>
2018-05-09 13:40:51 -0400

    Added rm flag to run script.

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ source ./server/config.sh
 
 docker run \
   -p 5432:5432 \
+  --rm \
   --name $DOCKER_IMAGE_NAME \
   docker.uncharted.software/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} \
   -d postgres
