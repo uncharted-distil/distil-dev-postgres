ff1f78cfbd30799670ca54e7d9a0ac07c29802ed
philippe horne <phorne@uncharted.software>
2020-06-03 14:15:08 -0400

    Updated to be able to use latest distil version. It means that ingest has to be built in the container for gdal support.

--- build.sh
+++ build.sh
@@ -7,10 +7,6 @@ NC='\033[0m'
 
 echo -e "${HIGHLIGHT}Getting distil-ingest..${NC}"
 
-# get distil-ingest and force a static rebuild of it so that it can run on Alpine
-env GOOS=linux GOARCH=amd64 CGO_ENABLED=0 go get -a -v github.com/uncharted-distil/distil-ingest/cmd/distil-ingest@$BRANCH
-mv $GOPATH/bin/distil-ingest ./server
-
 # copy the d3m data into the docker context
 echo -e "${HIGHLIGHT}Copying D3M data..${NC}"
 mkdir -p ./server/data
@@ -20,7 +16,7 @@ mv ./server/data/output ./server/data/d3m
 
 echo -e "${HIGHLIGHT}Building image ${DOCKER_IMAGE_NAME}...${NC}"
 cd server
-docker build --squash --no-cache --network=host \
+docker build --build-arg DISTIL_BRANCH=$BRANCH --squash --no-cache --network=host \
     -t $DOCKER_REPO/$DOCKER_IMAGE_NAME:${DOCKER_IMAGE_VERSION} -t $DOCKER_REPO/$DOCKER_IMAGE_NAME:latest .
 cd ..
 
